6fff832d8b1ca0981097533ca4bd27394bb18d78
ChinaHackers <93428739@qq.com>
2018-05-28 23:08:13 +0800

    init push.

---
+++ FlipClock/FlipClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlipClock
+//
+//  Created by Liu Chuan on 2018/5/28.
+//  Copyright © 2018年 LC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
